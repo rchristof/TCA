cfe91ae93935d5750bc2b103e79a409679da4e09
rchristof <rafael.dana.christof@gmail.com>
2024-12-17 04:16:27 -0300

    fix: change overridable method on testEntity constructor

--- src/br/com/guardiaosistemas/tca/execucao/model/entity/TestEntity.java
+++ src/br/com/guardiaosistemas/tca/execucao/model/entity/TestEntity.java
@@ -11,7 +11,7 @@ public class TestEntity {
     public TestEntity(int totalItems, int frequency, int soa, List<HitEntity> hitEntities) {
         this.totalItems = totalItems;
         this.frequency = frequency;
-        this.setSoa(soa);
+        this.soa = soa;
         this.hitEntities = hitEntities;
     }
 
